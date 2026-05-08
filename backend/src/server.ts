1f9a1e6c1b868ab9dd794556c195817adb9dad76
Jan Kokobane <mashajan1991@gmail.com>
2026-05-08 15:56:58 +0000

    feat(admin-auth): add backend for admin registration and login

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import authRoutes from './routes/authRoutes';
 import pool from './config/db';
 import { QueryResult } from 'pg';
+
+// Routes
+import authRoutes from './routes/authRoutes';
+import adminAuthRoutes from './routes/adminAuthRoutes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 pool.query('SELECT NOW()', (err: Error | null, res: QueryResult<any>) => {
   if (err) {
@@ -25,9 +29,9 @@ pool.query('SELECT NOW()', (err: Error | null, res: QueryResult<any>) => {
   }
 });
 
-// Routes
 app.use('/api/auth', authRoutes);
 
-// Start server
+app.use('/api/admin/auth', adminAuthRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
